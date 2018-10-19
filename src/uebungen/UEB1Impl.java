06528f05eb574d476a0eec80cc44ece9147ebb76
Alexander Tampier <16546792+alexander-tampier@users.noreply.github.com>
2018-10-19 12:45:41 +0200

    finished requirements for ue01 (#1) (#2)

--- src/uebungen/UEB1Impl.java
+++ src/uebungen/UEB1Impl.java
@@ -1,17 +1,17 @@
 package uebungen;
 
-import mywebserver.UrlImpl;
 import BIF.SWE1.interfaces.UEB1;
 import BIF.SWE1.interfaces.Url;
+import mywebserver.UrlImpl;
 
 public class UEB1Impl implements UEB1 {
 
-	@Override
-	public Url getUrl(String path) {
-		return new UrlImpl(path);
-	}
+  @Override
+  public Url getUrl(String path) {
+    return new UrlImpl(path);
+  }
 
-	@Override
-	public void helloWorld() {
-	}
+  @Override
+  public void helloWorld() {
+  }
 }
